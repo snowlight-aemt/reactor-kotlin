5bcdd30cb5213d59d65b9a3f44e1e0dd308c901e
snowlight-aemt <it.s.aemt@gmail.com>
2024-02-05 23:20:50 +0900

    Coroutine: Work (Abort Async Flow)

---
+++ src/main/kotlin/coroutine/WorkNotClosed.kt
@@ -0,0 +1,35 @@
+package me.snowlight.coroutine
+
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.newSingleThreadContext
+import kotlinx.coroutines.runBlocking
+import mu.KotlinLogging
+import kotlin.system.measureTimeMillis
+import kotlin.time.Duration.Companion.milliseconds
+import kotlin.time.Duration.Companion.seconds
+
+private val logger = KotlinLogging.logger {}
+private val single = newSingleThreadContext("worker")
+
+fun main() {
+    measureTimeMillis {
+        runBlocking {
+            launch(single) { workHard() }
+            launch(single) { workEasy() }
+        }
+    }.let { logger.debug {">> elasped : $it ms"} }
+}
+
+private suspend fun workHard() {
+    logger.debug { "start hard work " }
+//    delay(3.seconds)
+    while(true) { }
+    logger.debug { "end hard work " }
+}
+
+private suspend fun workEasy() {
+    logger.debug { "start easy work " }
+    delay(1.seconds)
+    logger.debug { "end easy work " }
+}
